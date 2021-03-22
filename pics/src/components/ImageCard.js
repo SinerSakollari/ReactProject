e4d98a086760cabe549163d12f96d8987ade87d0
Siner Sakollari <sinersakollari13@gmail.com>
2021-03-22 18:32:27 +0100

    update 3/22

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import Reacr from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+        
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
-        const {description, url} = this.props.image;
+        const {description, urls} = this.props.image;
 
         return (
             <div>
                 <img 
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
